6479aaf50cf2394069e623c910e392ca01c06eb4
cdricn <cdricncdricn@gmail.com>
2026-04-10 16:20:21 +0800

    Redirect page to works

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,5 @@
-
-import styles from "./page.module.css";
-import Works from "./works/works";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-
-  return (
-    <>
-      <div className={styles['page-container']}>
-        <nav className={styles['nav-container']}>
-          <h1 className={styles['nav-header']}>HEAD</h1>
-          <ul className={styles['nav-options']}>
-            <li className={styles['nav-option']}>
-              WORKS
-              <span id={'works-line'} className={styles['option-line']}></span>
-            </li>
-            <li className={styles['nav-option']}>TEST</li>
-            <li className={styles['nav-option']}>ABOUT</li>
-          </ul>
-        </nav>
-        <Works />
-      </div>
-      <footer>
-
-      </footer>
-    </>
-  );
+  redirect("http://localhost:3000/works");
 }
