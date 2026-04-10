ead5ddcc2934fa7f37c74f49ffd3332e0409913d
cdricn <cdricncdricn@gmail.com>
2026-04-11 01:41:06 +0800

    Transfered span animation to its own component

--- app/mainNav/mainNav.tsx
+++ app/mainNav/mainNav.tsx
@@ -1,10 +1,40 @@
+'use client';
+
 import styles from "./mainNav.module.css";
 import Link from "next/link";
+import NavLine from "./line/navLine";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default async function mainNav() {
+export default function mainNav() {
+  const [containerProperties, setContainerProperties] = useState({
+    containerWidth: 0,
+    optionWidth: 0,
+    optionHeight: 0,
+    optionPosY: 0
+  });
+
+  // Returns with /[slug].
+  const params = usePathname(); 
+
+  useEffect(()=>{
+    const navOptionsID = document.getElementById('nav-options');
+    const optionID = document.getElementById(params.slice(1));
+
+    if (navOptionsID === null || optionID === null) return;
+
+    setContainerProperties({
+      containerWidth: navOptionsID.getBoundingClientRect().width,
+      optionWidth: optionID.getBoundingClientRect().width,
+      optionHeight: optionID.getBoundingClientRect().height,
+      optionPosY: optionID.offsetTop
+    });
+
+  }, []);
+  
 
   return (
-    <ul className={styles['nav-options']}>
+    <ul id={'nav-options'} className={styles['nav-options']}>
       <li id={'works'} className={styles['nav-option']}>
         <Link href="/works" className={styles['nav-item']}>WORKS</Link>
       </li>
@@ -14,7 +44,7 @@ export default async function mainNav() {
       <li id={'about'} className={styles['nav-option']}>
         <Link href="/about" className={styles['nav-item']}>ABOUT</Link>
       </li>
-      <span className={styles['option-line']}></span>
+      <NavLine containerProperties={containerProperties} />
     </ul>
   );
 }
