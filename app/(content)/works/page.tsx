2f3c4249d5c1bc902221943a77063d38b2f4d2b7
cdricn <cdricncdricn@gmail.com>
2026-04-13 14:22:19 +0800

    Moved main container to layout.tsx file

--- app/(content)/works/page.tsx
+++ app/(content)/works/page.tsx
@@ -12,19 +12,17 @@ export default async function Page() {
   const {data: works} = await sanityFetch({query: POSTS_QUERY});
 
   return (
-    <main className={styles['main-container']}>
-      <div className={styles['items-container']}>
-        {works.map((works:SanityDocument)=>{
-          const displayDate = works.publishedAt.slice(0,7).replace(/-/g, "."); //trim last three digits, turn - to .
-          return (
-            <div key={works._id} className={styles['item']}>
-              <span className={styles['date']}>{displayDate}</span>
-              <h2 className={styles['title']}>{works.title}</h2>
-              <span className={styles['type']}>{works.type}</span>
-            </div>
-          )})
-        }
-      </div>
-    </main>
+    <div className={styles['items-container']}>
+      {works.map((works:SanityDocument)=>{
+        const displayDate = works.publishedAt.slice(0,7).replace(/-/g, "."); //trim last three digits, turn - to .
+        return (
+          <div key={works._id} className={styles['item']}>
+            <span className={styles['date']}>{displayDate}</span>
+            <h2 className={styles['title']}>{works.title}</h2>
+            <span className={styles['type']}>{works.type}</span>
+          </div>
+        )})
+      }
+    </div>
   )
 } 
