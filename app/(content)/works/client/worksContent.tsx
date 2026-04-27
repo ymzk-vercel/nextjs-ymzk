1973d29a8b3cb952354844ef1e2d799083be0f16
cdricn <cdricncdricn@gmail.com>
2026-04-27 19:16:41 +0800

    Made separate client component for works items

---
+++ app/(content)/works/client/worksContent.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import styles from './worksContent.module.css';
+import WorksPopUp from '@/app/components/worksPopUp/worksPopUp';
+import { type Works } from '@/sanity/lib/types';
+import { useState } from 'react';
+
+export default function WorksContent({works}:{works: Works}) {
+  const [displayPopUp, setDisplayPopUp] = useState(false);
+
+  //trim last three digits, turn '-' to '.'
+  const displayDate = works.publishedAt.slice(0,7).replace(/-/g, "."); 
+
+  function setPopUp() {
+    setDisplayPopUp(!displayPopUp);
+  }
+
+  return (
+    <>
+      {displayPopUp ? <WorksPopUp displayPopUp={setPopUp} works={works}/> : null}
+      <div onClick={()=>setDisplayPopUp(!displayPopUp)} className={styles['item']}>
+        <span className={styles['date']}>{displayDate}</span>
+        <h2 className={styles['title']}>{works.title}</h2>
+        <span className={styles['type']}>{works.type}</span>
+      </div>
+    </>
+  )
+} 
