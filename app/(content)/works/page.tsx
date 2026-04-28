c1c31f7a331728a90af9717907f4a20a6b48c1ea
cdricn <cdricncdricn@gmail.com>
2026-04-28 22:15:46 +0800

    Separated works tsx into own client component

--- app/(content)/works/page.tsx
+++ app/(content)/works/page.tsx
@@ -1,6 +1,4 @@
-import styles from './page.module.css';
 import WorksContent from './client/worksContent';
-import { type Works } from '@/sanity/lib/types';
 import { sanityFetch } from '@/sanity/lib/live';
 
 const WORKS_QUERY = `*[
@@ -11,16 +9,7 @@ const WORKS_QUERY = `*[
 export default async function Page() {
   const {data} = await sanityFetch({query: WORKS_QUERY});
 
-  const style = {
-    animation: 'fadeIn .5s ease-in forwards'
-  };
-
   return (
-    <div style={style} className={styles['items-container']}>
-      {data ? data.map((works: Works)=> {
-          return (<WorksContent key={works._id} works={works}/>) 
-        }) : null
-      }
-    </div>
+    <WorksContent data={data} />
   )
 } 
